56064c806cc2968eb6feea74ae5c41e437911862
malte <malt0533@stud.kea.dk>
2022-05-20 13:55:15 +0200

    tent option started

--- src/components/Tickets/TentOptions/TentOptions.js
+++ src/components/Tickets/TentOptions/TentOptions.js
@@ -1,31 +1,56 @@
 import "./_TentOptions.scss";
+import { useContext, useState } from "react";
+import { OrderContext } from "../../../context/Tickets";
 
 function TentOptions() {
+	const [tentOption, setTentOption] = useState(0);
+	const { order, setOrder } = useContext(OrderContext);
+
+	const checked = (e) => {
+		setTentOption(e.target.value);
+	};
+
 	return (
-		<section className="section_p tents">
+		<section className="ticket_choice tent_choice">
 			<h2>Choose your tent option</h2>
-			<form>
+			<div>
 				<fieldset>
-					<input type="radio" name="tent" id="own_tent" value={1} defaultChecked />
-					<label htmlFor="own_tent">
-						Bring own tent <span>free</span>
-					</label>
+					<input type="radio" name="tent_option" value={"own"} defaultChecked onChange={checked} />
+					<p>
+						<strong>Bring Own Tent</strong>
+					</p>
 				</fieldset>
+				<fieldset>
+					<input type="radio" name="tent_option" value={"pre"} onChange={checked} />
+					<p>
+						<strong>Crew Setup</strong>
+					</p>
+				</fieldset>
+				<fieldset disabled={tentOption === "own" ? true : false}>
+					<button className="choice_btn">-</button>
+					<p>%</p>
+					<button className="choice_btn">+</button>
 
-				<fieldset>
-					<input type="radio" name="tent" id="green" value={2} />
-					<label htmlFor="green">
-						Green Camping <span>+249,-</span>
-					</label>
+					<p>
+						<strong>2 person tent 299,-</strong>
+					</p>
 				</fieldset>
+				<fieldset disabled={tentOption === "own" ? true : false}>
+					<button className="choice_btn">-</button>
+					<p>%</p>
+					<button className="choice_btn">+</button>
 
-				<fieldset>
-					<input type="radio" name="tent" id="crew" value={3} />
-					<label htmlFor="crew">
-						Crew set up <span>+299,- / 399,-</span>
-					</label>
+					<p>
+						<strong>3 person tent 399,</strong>
+					</p>
 				</fieldset>
-			</form>
+				<fieldset>
+					<input type="checkbox" />
+					<p>
+						<strong>Green Camping</strong>
+					</p>
+				</fieldset>
+			</div>
 		</section>
 	);
 }
--- src/context/Tickets.js
+++ src/context/Tickets.js
@@ -5,14 +5,26 @@ export const OrderContext = createContext();
 export const OrderProvider = ({ children }) => {
 	const [order, setOrder] = useState({
 		regular: 0,
+		regularPrice: 799,
 		vip: 0,
+		vipPrice: 1299,
 		area: { svartheim: 0, nilfheim: 0, helheim: 0, muspelheim: 0, alfheim: 0 },
 		guests: [],
 		tentOption: {
 			bringOwn: true,
-			green: null,
 			crewSetup: false,
+			green: null,
+			twoPersonPrice: 299,
+			threePersonPrice: 399,
 		},
+		crewTents: {
+			twoPerson: 0,
+			twoPersonPrice: 0,
+			threePerson: 0,
+			threePersonPrice: 0,
+			bookingfee: 99,
+		},
+		sum: 0,
 	});
 
 	const value = {
