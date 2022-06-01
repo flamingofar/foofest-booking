0193548f6a8e6c81e7a03b623adb6e9d66bd12be
malte <malt0533@stud.kea.dk>
2022-06-01 23:14:50 +0200

    cta to blue, cta disable color, sticky basket

--- src/components/Tickets/Basket/Basket.js
+++ src/components/Tickets/Basket/Basket.js
@@ -20,31 +20,33 @@ function Basket({ linkActive, guestsValid, setGuestsValid }) {
 
 	return (
 		<section className="basket">
-			<p onClick={handleBasket}>
-				Basket{" "}
-				{isMobile ? (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="16"
-						height="16"
-						fill="currentColor"
-						className="bi bi-basket3"
-						viewBox="0 0 16 16"
-					>
-						<path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM3.394 15l-1.48-6h-.97l1.525 6.426a.75.75 0 0 0 .729.574h9.606a.75.75 0 0 0 .73-.574L15.056 9h-.972l-1.479 6h-9.21z" />
-					</svg>
-				) : null}
-			</p>
+			<div className="sticky">
+				<p onClick={handleBasket}>
+					Basket{" "}
+					{isMobile ? (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							className="bi bi-basket3"
+							viewBox="0 0 16 16"
+						>
+							<path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM3.394 15l-1.48-6h-.97l1.525 6.426a.75.75 0 0 0 .729.574h9.606a.75.75 0 0 0 .73-.574L15.056 9h-.972l-1.479 6h-9.21z" />
+						</svg>
+					) : null}
+				</p>
 
-			{isMobile ? isOpen && isMobile && <BasketContent /> : <BasketContent />}
+				{isMobile ? isOpen && isMobile && <BasketContent /> : <BasketContent />}
 
-			<Link
-				disabled={guestsValid}
-				to={"/checkout"}
-				className={`desktop_checkout cta ${guestsValid ? "disabled" : ""}`}
-			>
-				Checkout
-			</Link>
+				<Link
+					disabled={guestsValid}
+					to={"/checkout"}
+					className={`desktop_checkout cta ${guestsValid ? "disabled" : ""}`}
+				>
+					Checkout
+				</Link>
+			</div>
 		</section>
 	);
 }
